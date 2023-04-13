220162923a445f1ec0225c0340e24069a1d719c2
lala-minxiong <66988775+lala-minxiong@users.noreply.github.com>
2023-04-13 12:44:38 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cilsjzz78xdy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ngminxiong-employee1"
+customregion = "us-east-1"
